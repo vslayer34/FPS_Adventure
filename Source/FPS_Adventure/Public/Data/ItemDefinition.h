ef63248bd95146070d16532c7be9fafb10c3a1eb
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2026-02-04 17:11:10 +0200

    Created ItemDefinition class from Asset Data

---
+++ Source/FPS_Adventure/Public/Data/ItemDefinition.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ItemData.h"
+#include "ItemDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class FPS_ADVENTURE_API UItemDefinition : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FName ID;
+
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FItemText ItemText;
+
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	TSoftObjectPtr<UStaticMesh> WorldMesh;
+};
